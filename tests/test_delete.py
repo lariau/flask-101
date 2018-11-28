8609b9465a2a03649b93f33bc4a74ae42b2a0332
christophe lariau <lariau@yahoo.fr>
2018-11-28 12:33:53 +0100

    delete added to product route

---
+++ tests/test_delete.py
@@ -0,0 +1,16 @@
+# tests/test_views.py
+from flask_testing import TestCase
+from wsgi import app
+
+class TestViews(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def test_delete_existing_product(self):
+        response = self.client.delete("/api/v1/products/1")
+        self.assertEqual(response.status_code,204)
+
+    def test_delete_unkown_product(self):
+        response = self.client.delete("/api/v1/products/5")
+        self.assertEqual(response.status_code,404)
